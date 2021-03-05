3905b426a53efb3521745dee175199910f1b4c9d
jlgge <jlgge@angkor>
2021-03-04 20:46:07 -0600

    Updated q1 to work with type parameterization, Not sure if this is right its just a copy of P5.

--- simple-refactoring-examples/src/q1/extract_method/refactored/A.java
+++ simple-refactoring-examples/src/q1/extract_method/refactored/A.java
@@ -2,37 +2,45 @@ package q1.extract_method.refactored;
 
 import java.util.List;
 
+//import p5.extract_method_typeparameterize.refactored.Graph;
+
 public class A {
    Node m1(List<Node> nodes, String p) {
-      // TODO: Your answer
+      extractedMethod(nodes, p);
       // other implementation
       return null;
    }
 
    Edge m2(List<Edge> edgeList, String p) {
-      // TODO: Your answer
+	   extractedMethod(edgeList, p);
       // other implementation
       return null;
    }
 
-   // TODO: Your answer
-   // ? extractedMethod(?) {
-   //
-   // }
+   /**
+    * Extract Method Refactoring by Type Parameterizing
+    */
+   <T extends Graph> void extractedMethod(List<T> objs, String p) {
+      for (T obj : objs) {
+         if (obj.contains(p))
+            System.out.println(obj);
+      }//end of for
+   }//end of extractedMethod
+
 }
 
-class Node {
-   String name;
+class Graph {
+	String name;
+	
+	public boolean contains(String p) {
+		return name.contains(p);
+	}
+}//end of class Graph
 
-   public boolean contains(String p) {
-      return name.contains(p);
-   }
+class Node extends Graph {
+ 
 }
 
-class Edge {
-   String name;
+class Edge extends Graph {
 
-   public boolean contains(String p) {
-       return name.contains(p);
-   }
 }
